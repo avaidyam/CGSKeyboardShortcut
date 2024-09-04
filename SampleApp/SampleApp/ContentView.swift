b2f6689b15671f1e088dba4ef100d6f9feefc283
Andres Kievsky <andres.kievsky@thoughtworks.com>
2024-09-04 11:27:01 +1000

    Add a sample delegate

--- SampleApp/SampleApp/ContentView.swift
+++ SampleApp/SampleApp/ContentView.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 struct SampleShortcutView: NSViewRepresentable {
     let placeholderString: String?
+    let delegate: KeyboardShortcutViewDelegate
     func makeNSView(context: Context) -> KeyboardShortcutView {
         let keyboardShortcutView = KeyboardShortcutView()
         keyboardShortcutView.placeholderString = placeholderString
+        keyboardShortcutView.delegate = delegate
         return keyboardShortcutView
     }
     
@@ -19,11 +21,30 @@ struct SampleShortcutView: NSViewRepresentable {
     }
 }
 
+class DelegateExample: KeyboardShortcutViewDelegate {
+    func keyboardShortcutViewShouldBeginRecording(_ keyboardShortcutView: KeyboardShortcutView) -> Bool {
+        true
+    }
+    
+    func keyboardShortcutView(_ keyboardShortcutView: KeyboardShortcutView, canRecordShortcut shortcut: KeyboardShortcutView.Pair) -> Bool {
+        true
+    }
+    
+    func keyboardShortcutViewDidEndRecording(_ keyboardShortcutView: KeyboardShortcutView) {
+        let text = keyboardShortcutView.shortcut.flatMap {
+            $0.modifierFlags.characters + $0.keyCode.characters
+        } ?? "nil"
+        print("Chosen shortcut was: \(text)")
+    }
+    
+}
+
 struct ContentView: View {
+    let delegateExample = DelegateExample()
     var body: some View {
         VStack {
             Text("Enter your shortcut:")
-            SampleShortcutView(placeholderString: "Shortcut")
+            SampleShortcutView(placeholderString: "Shortcut", delegate: delegateExample)
                 .frame(width: 100, height: 50)
                 .padding(.all, 20)
         }
